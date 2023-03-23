6c8ab6b6db3dcdd68c11d85eda6c647ad298ce16
AleksandrB00 <104912130+AleksandrB00@users.noreply.github.com>
2023-03-23 11:54:22 +0200

    Update bot_settings.py

--- settings/bot_settings.py
+++ settings/bot_settings.py
@@ -1,15 +1,15 @@
 from aiogram.types import LabeledPrice
 
-BOT_TOKEN = '2077557385:AAE5bczX6j9KIzccjZiisn5N1m1Zf-ZX750'
+BOT_TOKEN = 'Токен бота'
 
-API_ID = 29455784
-API_HASH = 'eb8c05b7791fadd64a4de3f8ced4a0dc'
+API_ID = Вашего приложения
+API_HASH = 'Вашего приложения'
 SESSION_NAME = 'My_session'
 
 QUERY = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 LIMIT_USER = 200
 
-PAYMENT_TOKEN = '401643678:TEST:8503e0e6-115f-4a63-83ca-56b007db18ee'
+PAYMENT_TOKEN = 'Токен оплаты'
 IMAGE_URL = 'https://www.meme-arsenal.com/memes/02e8c539849dc48732f942548501d987.jpg'
 
 PRICE = LabeledPrice(label='Подписка на 10 дней', amount=30000)
